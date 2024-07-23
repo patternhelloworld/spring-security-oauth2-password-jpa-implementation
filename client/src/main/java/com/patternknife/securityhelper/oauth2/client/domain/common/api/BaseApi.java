b33f99ac9d6f72c255f380b8a60e3c149f6ae0bb
Andrew-Kang-G <studypurpose@naver.com>
2024-07-23 11:14:18 +0900

    ci/cd : add samples to the client folder

--- client/src/main/java/com/patternknife/securityhelper/oauth2/client/domain/common/api/BaseApi.java
+++ client/src/main/java/com/patternknife/securityhelper/oauth2/client/domain/common/api/BaseApi.java
@@ -1,6 +1,7 @@
 package com.patternknife.securityhelper.oauth2.client.domain.common.api;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +11,18 @@ public class BaseApi {
     @Value("${spring.profiles.active}")
     private String activeProfile;
 
-
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/systemProfile")
-    public String getProfile () {
+    public String checkAuthenticated () {
         return activeProfile;
     }
+
+    @PreAuthorize("@customAuthorityService.hasAnyUserRole()")
+    @GetMapping("/systemProfile2")
+    public String checkPermissions () {
+        return activeProfile;
+    }
+
 
 
     @GetMapping("/")
